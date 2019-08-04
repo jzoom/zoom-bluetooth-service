aa45cf72fcb429b59a61ae936840d630622a34bb
JZoom <jzoom8112@gmail.com>
2019-08-04 18:04:29 +0800

    修改test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,47 @@
-import chai from 'chai';
-
+import BluetoothService from '../src/bluetooth_service';
 
 describe('test bluetooth serice', () => {
 
-  it('test bluetooth serice', () => {
-  
+  it('test bluetooth serice', async (done) => {
+    BluetoothService.setAdapter({
+      openBluetoothAdapter(){
+        return true;
+      },
+      startBluetoothDevicesDiscovery(){
+        return true;
+      },
+      onBluetoothDeviceFound(callback){
+
+      },
+      onBLECharacteristicValueChange(callback){
+        
+      }
+
+
+
+    });
+    var service = new BluetoothService([
+      {
+        filter(device){
+          return device.name.startsWith("ABC");
+        },
+        onCreateDevice(service,device){
+          return service.createBleDevice(device);
+        }
+      }
+    ]);
+
+    service.onDeviceFound((device)=>{
+      service.stopScan();
+    });
+
+    try{
+      await service.startScan();
+      done();
+    }catch(e){
+      throw e;
+    }
+    
   });
   
 });
